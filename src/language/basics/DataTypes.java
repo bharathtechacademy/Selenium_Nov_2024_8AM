e9939655e44652b4a68f8df8286e94ce40cf0cf0
bharathautomation <soft.ittech20@gmail.com>
2024-11-11 09:09:40 +0530

    Data Types

---
+++ src/language/basics/DataTypes.java
@@ -0,0 +1,36 @@
+package language.basics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		byte age = 32;
+		System.out.println(age);
+		
+		short year = 1993;
+		System.out.println(year);
+		
+		int empId= 45678967;
+		System.out.println(empId);
+		
+		long phone = 9553220022L;
+		System.out.println(phone);
+		
+		float percentage = 92.37f;
+		System.out.println(percentage);
+		
+		double latitude = 93.765478987;
+		System.out.println(latitude);
+		
+		boolean doYouHaveVisa = true;
+		System.out.println(doYouHaveVisa);
+		
+		char grade = 'A';
+		System.out.println(grade);
+		
+		String fullName = "Bharath Reddy";
+		System.out.println(fullName);
+
+	}
+
+}
