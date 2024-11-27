63466f4a26646b2f61c075d0aa64d86706305065
bharathautomation <soft.ittech20@gmail.com>
2024-11-27 10:10:04 +0530

    Non Access Modifiers

---
+++ src/abs/pack/AbstractClass1.java
@@ -0,0 +1,11 @@
+package abs.pack;
+
+public abstract class AbstractClass1 {
+	
+	//method ==> Collection of statements / Block of code
+	
+	public abstract void printNameAndId(String empName, int empId);	
+	
+	public abstract void printAddress() ;
+
+}
---
+++ src/abs/pack/AbstractClass2.java
@@ -0,0 +1,25 @@
+package abs.pack;
+
+public class AbstractClass2 extends AbstractClass1{
+	
+	
+
+	public static void main(String[] args) {
+		
+	}
+
+	@Override
+	public void printNameAndId(String empName, int empId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printAddress() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
